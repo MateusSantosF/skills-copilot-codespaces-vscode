13af34e68e1c43ec6a6c27733a5330451aee52e1
MateusSantosF <mateusferreira.dev@gmail.com>
2024-05-27 20:27:05 -0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+// Create web server
+// Load modules
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+var path = require('path');
+
+// Create server
+http.createServer(function (req, res) {
+    // Parse the request containing file name
+    var pathname = url.parse(req.url).pathname;
+    // Print the name of the file for which request is made.
+    console.log("Request for " + pathname + " received.");
+
+    // Read the requested file content from file system
+    fs.readFile(path.join(__dirname, pathname), function (err, data) {
+        if (err) {
+            console.log(err);
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            res.writeHead(404, {'Content-Type': 'text/html'});
+        } else {
+            // Page found
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, {'Content-Type': 'text/html'});
+
+            // Write the content of the file to response body
+            res.write(data.toString());
+        }
+        // Send the response body
+        res.end();
+    });
+}).listen(8080);
+
+// Console will print the message
+console.log('Server running at http://):8080/');
